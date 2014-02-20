7f99f5fcc5474616c673a1a82ffc416eaaaba16e
Luis Padrón <luisgpadronleon@gmail.com>
2014-02-20 14:22:19 -0430

    agredado de twits en footer

--- app/views/footer.blade.php
+++ app/views/footer.blade.php
@@ -57,8 +57,14 @@
 			</div>
 
 			<div class="onefourth footerTweets">
-				<h4>TWITTER</h4>
-				<div class="tweets"></div>
+				<div class="tweets" style="margin-top:20px;">
+                <a class="twitter-timeline" href="https://twitter.com/unisonido" data-widget-id="436571133710585856">Tweets por @unisonido</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+
+                
+                
+                </div>
 			</div>
 
 		</div>
