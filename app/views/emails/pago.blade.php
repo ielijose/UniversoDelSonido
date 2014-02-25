b8a55e1adfc17863215e779b47cc135ff62985f1
Eli José Carrasquero [PC] <ielijose@gmail.com>
2014-02-25 14:07:01 -0430

    Sistema de pagos

--- app/views/emails/pago.blade.php
+++ app/views/emails/pago.blade.php
@@ -5,8 +5,8 @@
 	<title>Factura</title>
 </head>
 <body style="font-family: sans-serif; background-color: #353637; margin: 0; color: #ecf0f1;"> 
-	<div style="width: 100%; background-color: #de2824; padding: 1em; text-align: center; border-bottom: 5px solid #c0392b;">
-		<img src="http://joelblackberryzone.com.ve/img/logo.png" alt="">
+	<div style="width: 100%; background-color: #f60; padding: 1em; text-align: center; border-bottom: 5px solid #c0392b;">
+		<img src="http://eluniversodelsonido.com.ve/admin/assets/img/logo.png" alt="">
 	</div>
 
 	<div style="padding: 0 1em;"> 
@@ -17,7 +17,7 @@
 		<p><strong>Recibo:</strong> {{ $pago['recibo'] }} </p>
 		<p><strong>Monto:</strong> {{ $pago['monto'] }} </p>
 		<p><strong>Fecha:</strong> {{ $pago['fecha'] }}</p>
-		<p><strong>Adjunto:</strong> <a href="http://joelblackberryzone.com.ve/{{ $pago['adjunto'] }}" style="font-size: 18px; background-color: #de2824; color: #ecf0f1; text-decoration: none; padding: .2em; border-bottom: 5px solid #c0392b; margin: 0 auto;">ENLACE</a> </p>
+		<p><strong>Adjunto:</strong> <a href="http://eluniversodelsonido.com.ve/{{ $pago['adjunto'] }}" style="font-size: 18px; background-color: #f60; color: #ecf0f1; text-decoration: none; padding: .2em; border-bottom: 5px solid #c0392b; margin: 0 auto;">ENLACE</a> </p>
 
 
 		<h1>Factura N# {{ $factura['id'] }} </h1>
@@ -55,7 +55,7 @@
 		<h4 align="right">Total: {{ $total }} Bs. </h4>
 	
 		<div align="center">
-			<a href="http://joelblackberryzone.com.ve/factura/{{ $factura['slug'] }}" style="font-size: 28px; background-color: #de2824; color: #ecf0f1; text-decoration: none; padding: .6em; border-bottom: 5px solid #c0392b; margin: 0 auto;"> Ver en linea </a>
+			<a href="http://eluniversodelsonido.com.ve/factura/{{ $factura['slug'] }}" style="font-size: 28px; background-color: #f60; color: #ecf0f1; text-decoration: none; padding: .6em; border-bottom: 5px solid #f50; margin: 0 auto;"> Ver en linea </a>
 		</div>
 	</div>
 	
--- app/views/factura.blade.php
+++ app/views/factura.blade.php
@@ -1,160 +1,200 @@
-@extends('layouts.master')
+@extends('layouts.public')
+
+@section('slider')
+<section id="noslider" class="sixteen columns headerContent" data-layout="blur">
+
+	<div id="blurMask">
+		<canvas id="blurCanvas"></canvas>
+	</div>
+
+	<div class="headerContentContainer">
+		<div class="pageTitle">Tu compra</div> 
+	</div>           
+</section>
+@stop
+
 @section('content')
-<div class="max-width">
-	<!-- Start row1 -->
-	<div class="row">		
-		<div class="col-lg-12 bg5">
+
+
+
+<div class="contentBgFull"></div>
+
+<!-- Tag Line ================================================== -->
+
+<div id="tagLineShadow" class="sixteen columns"></div>
+
+<!-- Portfolio items ================================================== -->
+<?php $cart = $factura->items; $total = 0; ?>
+@if(count($cart) > 0)
+
+<section class="sixteen columns row-fourty left-twenty comparison">
+	<article class="onefourth">
+		<ul class="features">
+			<?php $i = -1; ?>
+			@foreach($cart as $item)
+			<?php $i++; $class= (($i%2)==0) ? "odd": "" ; ?>
+			<li class="{{ $class }}">  {{ $item->producto->nombre }}  </li>
+			@endforeach
+		</ul>
+	</article>
+
+	<article class="onefourth">
+		<header><h4>CANTIDAD</h4></header>
+		<ul class="features">
+			<?php $i = -1; ?>
+			@foreach($cart as $item)
+			<?php $i++;  $class= (($i%2)==0) ? "odd": "" ; ?>
+			<li class="{{ $class }}">
+				<span class="cart-qty">{{ $item->cantidad }}</span>
+			</li>
+			@endforeach
+		</ul>
+	</article>
+
+	<article class="onefourth">
+		<header><h4>PRECIO</h4></header>
+		<ul class="features" style="font-weight:bold; color:#F30; font-size:16px;">
+			<?php $i = -1; ?>
+			@foreach($cart as $item)
+			<?php $i++;  $class= (($i%2)==0) ? "odd": "" ; ?>
+			<li class="{{ $class }}">Bs.  {{ $item->precio }}  </li>
+			@endforeach
+		</ul>
+	</article>
+
+	<article class="onefourth last">
+		<header class=""><h4>SUBTOTAL</h4></header>
+		<ul class="features" style="font-weight:bold; color:#C00; font-size:16px;">
+			<?php $i = -1; ?>
+			@foreach($cart as $item)
+			<?php $i++; $class= (($i%2)==0) ? "odd": "" ; ?>
+			<li class="{{ $class }}">Bs.   {{ $item->precio*$item->cantidad }}  </li>
+			<?php $total += $item->precio*$item->cantidad; ?>
+			@endforeach
+		</ul>
+	</article>
+
+	<article class="onefourth" style="float:right">		
+		<header class="highlighted"><h4>TOTAL</h4></header>
+		<ul class="features" style="font-weight:bold; color:#F00; font-size:18px;">
+			<li>Bs.  {{ $total }} </li>
+		</ul>
+	</article>
+
+	
+</section>
+
+<section class="sixteen columns offset-by-one">
+
+	<div class="sectionHeader row">
+
+		<div class="sectionHeadingWrap">
+			<span class="sectionHeading">Datos de Facturación</span>
+		</div>
+
+	</div>
+
+
+   <!-- Columns - One half
+   ================================================== -->
+   
+   <section class="contactForm" style="background-color:#f8f8f8; padding:10px;">
+
+   	<div id="" class="quote">
+
+   		{{ Form::open(array('url' => '/pagar', 'method' => 'post', 'id' => 'pagar', 'enctype' => 'multipart/form-data')) }}
+			<!-- Start billing address -->
+			<div class="shipping-address bg5">
 				<!-- Start heading -->
 				<div class="row">
-					@if($factura->pago)
-						<br/>
-						<h1 align="center">Datos de pago enviados, te responderemos a la brevedad posible.</h1>
-					@endif
 					<div class="col-lg-12 bg2 heading pull-left">
-						<h4>Mi Orden de compra:</h4>			
+						<h4>Datos de facturación</h4>			
 					</div>		
 				</div>			
 				<!-- End heading -->
-				@if(count($factura->items) > 0)
-				<!-- Start Shopping Cart -->
-				<div class="shopping-cart bg3">
-					<table class="table marginbot-clear">
-						<thead>
-							<tr>
-								<th>No</th>
-								<th>Producto</th>
-								<th>Cantidad</th>
-								<th>Precio</th>
-							</tr>
-						</thead>
-						<tbody>
-							<?php $no=0; $total = 0; ?>
-							@if(count($factura->items) > 0)
-							@foreach($factura->items as $item)
-							<?php $no++; $total+=$item->precio; ?>
-							<tr>
-								<td>{{ $no }}. </td>
-								<td>
-									<a href="#"><img src="{{ $item->producto->imagen->imagen }}" class="cart-img" alt=""></a>
-									<a href="#" class="item-name">{{ $item->producto->nombre }}</a>
-								</td>
-								<td> 
-									<input type="text" class="cart-qty" value="{{ $item->cantidad }}" disabled />
-								</td>
-								<td>{{ $item->precio }} Bs</td>
-							</tr>
-							@endforeach
-							@endif
 
-							<tr>
-								<td colspan="3" class="cart-bottom">Subtotal :</td>
-								<td>{{ $total }} Bs.</td>													
-							</tr>
+				<div class="box">
+					@if($factura->pago)						
+						<ul class="listForm">
+							<li>	
+							{{ Form::label('recibo', 'Numero de recibo o transferencia: ') }}
+								{{ $factura->pago->recibo }}
+							</li>
+							<li>
+							{{ Form::label('monto', 'Monto: ')}}
+								{{ $factura->pago->monto }}
+								</li>
+							<li>
+							{{ Form::label('fecha', 'Fecha: ') }}
+								{{ $factura->pago->fecha }}
+							</li>
+							<li>
+							{{ Form::label('adjunto', 'Adjunto: ') }}
+							<a href="/{{ $factura->pago->adjunto }}" target="_popup">ADJUNTO</a>	
+							</li>							
+						</ul>
+					@else
+						<div class="form-horizontal ">
+							<ul class="listForm">
+								{{ Form::label('recibo', 'Numero de recibo o transferencia: ', array('class' => 'col-lg-2 control-label')) }}
+								<li class="col-lg-8">
+									{{ Form::text('recibo', null, array('class' => 'form-block', 'placeholder' =>'Numero de recibo o transferencia', 'required' => "required")) }}
+									<div class="validation"></div>
+								</li>
 
-							<tr>
-								<td colspan="3" class="text-right bg1"></td>
-								<td></td>
-							</tr>							  
-						</tbody>
-					</table>						
-				</div>				
-				<!-- End Shopping Cart -->
+								{{ Form::label('monto', 'Monto: ', array('class' => 'col-lg-2 control-label')) }}
+								<li class="col-lg-8">
+									{{ Form::text('monto', null, array('class' => 'form-block', 'placeholder' =>'Monto.', 'required' => "required" )) }}
+									<div class="validation"></div>
+								</li>
+
+								{{ Form::label('fecha', 'Fecha: ', array('class' => 'col-lg-2 control-label')) }}
+								<li class="col-lg-8">
+									{{ Form::text('fecha', null, array('class' => 'form-block', 'placeholder' =>'dd/mm/aaaa', 'required' => "required" )) }}
+									<div class="validation"></div>
+								</li>
 
 
-				{{ Form::open(array('url' => '/pagar', 'method' => 'post', 'id' => 'pagar', 'class' => 'validateform', 'enctype' => 'multipart/form-data')) }}
-					<!-- Start billing address -->
-					<div class="shipping-address bg5">
-						<!-- Start heading -->
-						<div class="row">
-							<div class="col-lg-12 bg2 heading pull-left">
-								<h4>Datos de facturación</h4>			
-							</div>		
-						</div>			
-						<!-- End heading -->
+								{{ Form::label('adjunto', 'Adjunto: ', array('class' => 'col-lg-2 control-label')) }}
+								<li class="col-lg-8">
+									<input name="adjunto" type="file" required="required" />
 
-						<div class="box">
-
-							@if($factura->pago)
+									<div class="validation"></div>
+								</li>
 								
-								<p class="listForm success-lf">
-										{{ Form::label('recibo', 'Numero de recibo o transferencia: ') }}
-											{{ $factura->pago->recibo }}
+							</ul>																
+						</div>	
+					@endif	
+				</div>
+			</div>
+			<!-- End billing address -->
+			@if(!$factura->pago)
+			<!-- Start cart action -->
+			<div class="row">
+				<div class="col-lg-12 bg2 cart-action">
+					<input type="submit" class="submit button dark reverted" id="submit" value="Confirmar pago" style="background-color:#f60; margin-top:0px; margin-right:30px;">
+				</div>		
+			</div>
+			@endif
 
-											<br/>
-
-										{{ Form::label('monto', 'Monto: ')}}
-											{{ $factura->pago->monto }}
-											<br/>
-
-										{{ Form::label('fecha', 'Fecha: ') }}
-											{{ $factura->pago->fecha }}
-											<br/>
-
-		
-										{{ Form::label('adjunto', 'Adjunto: ') }}
-										<a href="/{{ $factura->pago->adjunto }}" target="_popup">ADJUNTO</a>
-										
-									</p>	
+			{{ Form::hidden('id', $factura->id )}}
+		{{ Form::close() }}	
 
 
-							@else
 
-								<div class="form-horizontal margintop20">
+   	</div>
 
-									<ul class="listForm">
-										{{ Form::label('recibo', 'Numero de recibo o transferencia: ', array('class' => 'col-lg-2 control-label')) }}
-										<li class="col-lg-8">
-											{{ Form::text('recibo', null, array('class' => 'form-block', 'placeholder' =>'Numero de recibo o transferencia', 'data-rule' => "required", 'data-msg' => "Campo obligatorio." )) }}
-											<div class="validation"></div>
-										</li>
+   </section>
 
-										{{ Form::label('monto', 'Monto: ', array('class' => 'col-lg-2 control-label')) }}
-										<li class="col-lg-8">
-											{{ Form::text('monto', null, array('class' => 'form-block', 'placeholder' =>'Monto.', 'data-rule' => "required", 'data-msg' => "Campo obligatorio." )) }}
-											<div class="validation"></div>
-										</li>
 
-										{{ Form::label('fecha', 'Fecha: ', array('class' => 'col-lg-2 control-label')) }}
-										<li class="col-lg-8">
-											{{ Form::text('fecha', null, array('class' => 'form-block', 'placeholder' =>'dd/mm/aaaa', 'data-rule' => "required", 'data-msg' => "Campo obligatorio." )) }}
-											<div class="validation"></div>
-										</li>
+</section>
 
-		
-										{{ Form::label('adjunto', 'Adjunto: ', array('class' => 'col-lg-2 control-label')) }}
-										<li class="col-lg-8">
-											<input name="adjunto" type="file" required="required" />
 
-											<div class="validation"></div>
-										</li>
-										
-									</ul>																
-								</div>	
-							@endif	
-						</div>
-					</div>
-					<!-- End billing address -->
-					@if(!$factura->pago)
-					<!-- Start cart action -->
-					<div class="row">
-						<div class="col-lg-12 bg2 cart-action">
-							<button type="submit" class="btn btn-lg btn-primary">Procesar compra</button>
-						</div>		
-					</div>
-					@endif
+@else
+	<section class="sixteen columns row-fourty left-twenty comparison">
+		<h2 align="center">No hay productos en tu carrito de compras :(</h2>
+	</section>
+@endif
 
-					{{ Form::hidden('id', $factura->id )}}
-				{{ Form::close() }}	
-				<!-- End cart action -->
-				@else
-				<br/>
-					<h2 align="center">No hay productos en tu carrito de compras :(</h2>
-				@endif
-		</div>				
-	</div>			
-	<!-- End row1 -->	
-	
-	<!-- Start social network -->
-	<!-- End social network -->				
-</div>
+<div class="clearfix"></div>
 @stop
